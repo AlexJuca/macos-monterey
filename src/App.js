43b79dee8e96b602293e7c5acdee3c7a912e0bf5
Alexandre Juca <corextechnologies@gmail.com>
2021-08-12 20:24:22 +0100

    implemented Process class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React, {Component} from 'react'
 import './App.scss';
-import Dock from './dock/Dock'
+import Dock from './components/dock/Dock'
+import StatusBar from './components/statusbar/StatusBar'
+import ProcessManager from './apps/manager/ProcessManager'
 
-function App() {
-  return (
-    <Dock />
-  );
+import Process from './lib/darwin/Process'
+
+class App extends Component {
+  main = new Process()
+  dock = new Process()
+  view = null
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      processes: [
+        {
+          name: 'dock',
+          pId: '1',
+          user: "AlexJuca",
+          executionTime: 90,
+        }
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+          <StatusBar />
+          <ProcessManager processes={this.state.processes} />
+          <Dock />
+      </div>
+    );
+  }
 }
 
 export default App;
