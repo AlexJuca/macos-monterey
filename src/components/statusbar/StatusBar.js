18c2a83ff73b183620b73d66b985f60fca9992b9
Alexandre Juca <corextechnologies@gmail.com>
2021-08-13 08:34:59 +0100

    implement macos statusbar

--- src/components/statusbar/StatusBar.js
+++ src/components/statusbar/StatusBar.js
@@ -11,9 +11,14 @@ class StatusBar extends Component {
             <div className="status-bar">
                 <div>
                     <ul className="status-bar-menu">
-                        <li>Finder</li>
-                        <li>File</li>
-                        <li>Edit</li>
+                        <li className="staus-bar-menu-option"><img className="status-bar-apple-icon" alt="apple-logo" src="../img/apple-status-bar-icon.png" /></li>
+                        <li className="staus-bar-menu-option">Finder</li>
+                        <li className="staus-bar-menu-option">File</li>
+                        <li className="staus-bar-menu-option">Edit</li>
+                        <li className="staus-bar-menu-option">View</li>
+                        <li className="staus-bar-menu-option">Go</li>
+                        <li className="staus-bar-menu-option">Window</li>
+                        <li className="staus-bar-menu-option">Help</li>
                     </ul>
                 </div>
             </div>
