ae9d925f2372119c532454e52b832efa8446bcb8
Alexandre Juca <corextechnologies@gmail.com>
2021-08-14 05:52:14 +0100

    implement button

---
+++ src/components/buttons/Button.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './button.scss'
+
+export default class Button extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const {text, onClick} = this.props
+        return (
+            <div onClick={onClick} className="darwin-button-large-primary">{text}</div>
+        )
+    }
+}
