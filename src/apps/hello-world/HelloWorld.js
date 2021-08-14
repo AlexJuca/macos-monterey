978fb4b3819776c5ef4e01b76a182b35f81a960d
Alexandre Juca <corextechnologies@gmail.com>
2021-08-14 07:36:35 +0100

    add should render prop and remove iframe

--- src/apps/hello-world/HelloWorld.js
+++ src/apps/hello-world/HelloWorld.js
@@ -7,6 +7,7 @@ class HelloWorld extends Window {
         super(props)
         this.state = {
             processes: props.processes,
+            should_render: true
         }
     }
 
@@ -23,9 +24,7 @@ class HelloWorld extends Window {
         return this.build(
             <div>
                 <p>This is a stupid simple application that plays a specific video, Enjoy the song!</p>
-                <div>
-                <iframe width="512" height="320" src="https://www.youtube.com/embed/5i6A1IHAQsg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
+            
                 <Button onClick={this.onClick} text={"Click me!!"} />
             </div>
         )
