345c7e2512e77dc274b0a87ce550af57df73adb1
Alexandre Juca <corextechnologies@gmail.com>
2021-08-14 05:53:00 +0100

    add button and yotube iframe to app

--- src/apps/hello-world/HelloWorld.js
+++ src/apps/hello-world/HelloWorld.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import Table from '../../components/table/Table'
+import Button from '../../components/buttons/Button'
 import Window from '../../components/window/Window'
 
 class HelloWorld extends Window {
@@ -15,9 +15,19 @@ class HelloWorld extends Window {
         this.setWindowFixedSize(true)
     }
 
+    onClick = () => {
+        alert("Button seems to be working!")
+    }
+
     render() {
         return this.build(
-            <p>Hello, World</p>
+            <div>
+                <p>This is a stupid simple application that plays a specific video, Enjoy the song!</p>
+                <div>
+                <iframe width="512" height="320" src="https://www.youtube.com/embed/5i6A1IHAQsg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <Button onClick={this.onClick} text={"Click me!!"} />
+            </div>
         )
     }
 }
