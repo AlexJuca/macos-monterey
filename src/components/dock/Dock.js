7d031cd3c7b32361b8acbb9cdbe06cf140bb7ed1
Alexandre Juca <corextechnologies@gmail.com>
2021-08-16 17:16:29 +0100

    implemented feature: Dock should show apps that were executed outside the dock

--- src/components/dock/Dock.js
+++ src/components/dock/Dock.js
@@ -21,86 +21,110 @@ class Dock extends Component {
             height: "72px",
             max_items: 13,
             visible: true,
+            undocked_applications_in_execution: [
+                [
+                    "trex", {
+                        icon: "/img/trex.png"
+                    }
+                ]
+            ],
+            docked_applications: [
+                [
+                    "finder", {
+                        icon: "img/finder.png",
+                    }
+                ],
+                [
+                    "launchpad", {
+                        icon: "img/launchpad.png",
+                        is_launcher: true,
+                    }
+                ],
+                [
+                    "slack", {
+                        icon: "img/slack.png"
+                    }
+                ],
+                [
+                    "chrome", {
+                        icon: "img/chrome.png"
+                    }
+                ],
+                [
+                    "imessage", {
+                        icon: "img/imessage.png"
+                    }
+                ],
+                [
+                    "notion", {
+                        icon: "img/notion.png"
+                    }
+                ],
+                [
+                    "safari", {
+                        icon: "img/safari.png"
+                    },
+                ],
+                [
+                    "terminal", {
+                        icon: "img/terminal.png"
+                    }
+                ],
+                [
+                    "calender", {
+                        icon: "img/calendar.png"
+                    }
+                ],
+                [
+                    "jetbrains-toolbox", {
+                        icon: "img/jetbrains-toolbox.png"
+                    }
+                ],
+                [
+                    "Maps", {
+                        icon: "img/maps.png"
+                    }
+                ],
+                [
+                    "sketch", {
+                        icon: "img/sketch.png"
+                    }
+                ]
+            ],
+
         }
     }
-    render() {
-        const applications = [
-            [
-                "finder", {
-                    icon: "img/finder.png",
-                }
-            ],
-            [
-                "launchpad", {
-                    icon: "img/launchpad.png",
-                    is_launcher: true,
-                }
-            ],
-            [
-                "slack", {
-                    icon: "img/slack.png"
-                }
-            ],
-            [
-                "chrome", {
-                    icon: "img/chrome.png"
-                }
-            ],
-            [
-                "imessage", {
-                    icon: "img/imessage.png"
-                }
-            ],
-            [
-                "notion", {
-                    icon: "img/notion.png"
-                }
-            ],
-            [
-                "safari", {
-                    icon: "img/safari.png"
-                },
-            ],
-            [
-                "terminal", {
-                    icon: "img/terminal.png"
-                }
-            ],
-            [
-                "calender", {
-                    icon: "img/calendar.png"
-                }
-            ],
-            [
-                "jetbrains-toolbox", {
-                    icon: "img/jetbrains-toolbox.png"
-                }
-            ],
-            [
-                "Maps", {
-                    icon: "img/maps.png"
-                }
-            ],
-            [
-                "sketch", {
-                    icon: "img/sketch.png"
-                }
-            ]
-        ]
 
-        const icons = applications.map((row, index) => {
+    render() {
+
+        const currently_docked_apps = this.state.docked_applications.map((row, index) => {
             if (index < this.state.max_items) {
                 return <DockIcon is_launcher={row[1].is_launcher} name={row[0]} key={index} icon={row[1].icon} />
             }
         })
 
+        const currently_running_apps = this.state.undocked_applications_in_execution.map((row, index) => {
+            if (index < this.state.max_items && this.state.undocked_applications_in_execution.length != 0 ) {
+                return <DockIcon is_launcher={row[1].is_launcher} name={row[0]} key={index} icon={row[1].icon} />
+            }
+        })
+
+        const conditionally_add_dock_seperator = () => {
+            if (this.state.undocked_applications_in_execution.length == 0) {
+                return null
+            } else {
+                return <div className="dock-seperator"></div>
+            }
+        }
+
         return (<div className="dock">
             <DockIconWrapper>
-                {icons}
+                {currently_docked_apps}
+                {conditionally_add_dock_seperator()}
+                {currently_running_apps}
             </DockIconWrapper>
         </div>)
     }
 }
-
 
 export default Dock
